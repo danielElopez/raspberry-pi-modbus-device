c3de9e7c1207e14c9297d30e9ec6759af6e6822e
danielElopez <dlopez@osisoft.com>
2019-06-17 13:32:15 -0500

    Removed more illegal chars

--- RaspberryPiModbusTCPTemperatureSensor.py
+++ RaspberryPiModbusTCPTemperatureSensor.py
@@ -2,10 +2,10 @@
 #  "AS IS" without any warranty of any kind.  OSIsoft DISCLAIMS ALL EXPRESS AND IMPLIED WARRANTIES,
 #  INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
 #  PURPOSE and NONINFRINGEMENT. In no event will OSIsoft be liable to Licensee or to any third party
-#  for damages of any kind arising from Licensee’s use of the this code OR OTHERWISE, including
+#  for damages of any kind arising from Licensee's use of the this code OR OTHERWISE, including
 #  but not limited to direct, indirect, special, incidental and consequential damages, and Licensee
 #  expressly assumes the risk of all such damages.  FURTHER, THIS CODE IS NOT ELIGIBLE FOR
-#  SUPPORT UNDER EITHER OSISOFT’S STANDARD OR ENTERPRISE LEVEL SUPPORT AGREEMENTS
+#  SUPPORT UNDER EITHER OSISOFT'S STANDARD OR ENTERPRISE LEVEL SUPPORT AGREEMENTS
 
 # Credit to Maxim for the work done here:
 # https://github.com/uzumaxy/pymodbus3
